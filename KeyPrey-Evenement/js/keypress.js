8e3a6696dc58816d15791463c8e82f8e0e8ffb1d
sounkalo20 <sidibesounk2003@gmail.com>
2022-12-05 10:12:49 +0000

    fin de l'exo

--- KeyPrey-Evenement/js/keypress.js
+++ KeyPrey-Evenement/js/keypress.js
@@ -2,16 +2,34 @@ let max = 180;
 
 $(document).ready(function () {
   $("#message").keydown(function (e) {
-    $("#lastKey").text("le code asii de la derniere lettre est " + e.which);
-    if(e.which==8){
-      if(max<180){
-          $("#charactersLeft").text(++max + " caracteres"); 
-      }
-    }else{
-      if(max>0){
-        $("#charactersLeft").text(--max + " caracteres");
-      }
+    if(e.which!=8){
+      maxL(this,180);
     }
-    
+    clavier(e);
   });
 });
+
+function maxL(element, max){
+  value = element.value;
+  max = parseInt(max);
+  if(value.length > max){
+      element.value = value.substr(0, max);
+      $("section").addClass("active");
+      $(".close-btn").click(function (e) { 
+        $("section").removeClass("active");
+      });
+  }
+}
+
+function clavier(event){
+  $("#lastKey").text("le code asii de la derniere lettre est " + event.which);
+if(event.which==8){
+  if(max<180){
+      $("#charactersLeft").text(++max + " caracteres"); 
+  }
+}else{
+  if(max>0){
+    $("#charactersLeft").text(--max + " caracteres");
+  }
+}
+}
